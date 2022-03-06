7c3d433231d15d83b8d7a2474a4ab915b24586ff
pabloduran <pdrm56@gmail.com>
2022-03-06 18:58:45 +0100

    Build package

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spy4cast",
+    version="0.0.1",
+    author="Pablo Duran",
+    author_email="pdrm56@gmail.com",
+    description="Python API for applying methodologies to .nc Datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pabloduran016/Spy4Cast",
+    project_urls={
+        "Repo": "https://github.com/pabloduran016/Spy4Cast",
+        "Documentation": "https://spy4cast-docs.netlify.app",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "spy4cast"},
+    packages=setuptools.find_packages(where="spy4cast"),
+    python_requires=">=3.6",
+)
