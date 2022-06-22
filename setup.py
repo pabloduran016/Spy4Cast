8c9798c16d288cd380da98ebca31e881f8ddf5db
pabloduran <pdrm56@gmail.com>
2022-06-22 15:26:45 +0200

    Update reqs

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where=""),
     install_requires=[
-        'numpy', 'matplotlib', 'xarray', 'dask', 'pandas', 'netcdf4'
+        'numpy', 'matplotlib', 'xarray', 'dask', 'pandas', 'netcdf4', 'cartopy'
     ],
     python_requires=">=3.6",
 )
